23e163c8112535131e92f10460d8caf96a9ce3cf
Sankalp Gupta <sankalpgupta444@gmail.com>
2024-03-03 18:50:25 +0530

    Database changed

--- main/settings.py
+++ main/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Electrothon6.0',
+        'USER': 'postgres',
+        'PASSWORD': '212004',
+        'HOST': 'localhost',  # Set to the PostgreSQL server's address
+        'PORT': '5432', 
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
