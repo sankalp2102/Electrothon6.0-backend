e547f30c96c5f61d541eb780f784f38d7519b4a7
Sankalp Gupta <sankalpgupta444@gmail.com>
2024-03-10 01:41:46 +0530

    Error resolved

--- roadmaps/urls.py
+++ roadmaps/urls.py
@@ -6,8 +6,8 @@ from .views import *
 
 urlpatterns = [
     path('subpages/', SubPageListView.as_view(), name='subpage-list'),
-    path('subpages/<str:subpage_name>/', ImageViews.as_view(), name='image-list'),
     path('subpages/search/', views.searchfunction, name='subpage-search'),
+    path('subpages/<str:subpage_name>/', ImageViews.as_view(), name='image-list'),
     path('blog/',blogViews.as_view(), name= 'blog-list'),
     path('blog/<int:blog_id>/report/', ReportblogViews.as_view(), name = 'report-blog'),
     path('teamdata/', TeamView.as_view(), name = 'team-list'),
