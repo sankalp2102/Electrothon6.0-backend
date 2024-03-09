564b05af352b3a278d834484e97c24d29be65b1b
Sankalp Gupta <sankalpgupta444@gmail.com>
2024-03-09 15:38:28 +0530

    Setings updated

--- roadmaps/views.py
+++ roadmaps/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.generics import ListAPIView
 from django.db import IntegrityError
 from .models import *
 from .serializers import *
