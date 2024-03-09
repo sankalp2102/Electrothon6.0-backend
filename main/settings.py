9916732d1a86b13bfa07500acccda5913ecc4992
Sankalp Gupta <sankalpgupta444@gmail.com>
2024-03-10 03:40:22 +0530

    CORS

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fe@sjwn*&e@+9ey$b_dto_%zhx21mx*lxx8z-ew2de%dsj-py_
 DEBUG = True
 
 ALLOWED_HOSTS = [
-   'electrothon6-0-backend.onrender.com',
+   # 'electrothon6-0-backend.onrender.com',
 ]
 
 
@@ -60,6 +60,15 @@ MIDDLEWARE = [
 #cors origins
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5500']
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 ROOT_URLCONF = 'main.urls'
 
