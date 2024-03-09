c82881d384bdb483cd10e77960b8d836d7522ef7
Sankalp Gupta <sankalpgupta444@gmail.com>
2024-03-09 11:33:46 +0530

    Urls updated

--- roadmaps/urls.py
+++ roadmaps/urls.py
@@ -5,7 +5,7 @@ from .views import SubPageListView, ImageViews, searchfunction, blogViews, TeamV
 urlpatterns = [
     path('subpages/', SubPageListView.as_view(), name='subpage-list'),
     path('subpages/<str:subpage_name>/', ImageViews.as_view(), name='image-list'),
-    path('subpage/search', searchfunction.as_view(), name='subpage-search'),
+    path('subpages/search/', searchfunction.as_view(), name='subpage-search'),
     path('blog/',blogViews.as_view(), name= 'blog-list'),
     path('blog/<int:blog_id>/report/', ReportblogViews.as_view(), name = 'report-blog'),
     path('teamdata/', TeamView.as_view(), name = 'team-list'),
