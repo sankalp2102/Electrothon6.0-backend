162bd94b664f2731070c82af92480acfc70a39bb
Sankalp Gupta <sankalpgupta444@gmail.com>
2024-03-10 08:26:58 +0530

    Some changes

--- main/settings.py
+++ main/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -59,7 +59,6 @@ MIDDLEWARE = [
 
 #cors origins
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5500']
 
 CORS_ALLOW_METHODS = [
     'DELETE',
